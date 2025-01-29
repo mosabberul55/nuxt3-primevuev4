0557ab81938c065bc2ad91ee4e038f67238d523b
Mosabberul Islam <76517232+mosabberul55@users.noreply.github.com>
2025-01-29 11:41:26 +0600

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+import Aura from '@primevue/themes/aura';
+
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    ssr: true,
+    modules: [
+      '@nuxtjs/tailwindcss',
+      '@pinia/nuxt',
+      '@primevue/nuxt-module',
+      '@vee-validate/nuxt',
+      '@nuxt/icon'
+    ],
+    primevue: {
+        autoImport: true,
+        usePrimeVue: true,
+        options: {
+            theme: {
+                preset: Aura,
+                // cssLayer: false,
+                options: {
+                    darkModeSelector: '.my-app-dark',
+                    cssLayer: {
+                        name: 'primevue',
+                        order: 'tailwind-base, primevue, tailwind-utilities'
+                    }
+                },
+            },
+            ripple: true,
+        },
+    }
+})
